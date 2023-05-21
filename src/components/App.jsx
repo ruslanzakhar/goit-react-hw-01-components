c4c8dc42279a3d1d884a4f26ec9e64927ad0f3bd
ruslanzakhar <zakharchuk_ruslan@icloud.com>
2023-05-21 23:45:20 +0300

    add-profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
+import user from './Profile/user.json';
+import Profile from './Profile/Profile';
+
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />; */}
     </div>
   );
-};
+}
