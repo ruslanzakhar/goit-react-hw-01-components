91703d545f7b36a9c100c7c93169fce13bbdd1b8
ruslanzakhar <zakharchuk_ruslan@icloud.com>
2023-05-25 19:01:45 +0300

    hw-1-react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,14 @@
 import user from './Profile/user.json';
 import Profile from './Profile/Profile';
 
+import data from './Statistics/data.json';
+import Statistics from './Statistics/Statistics';
+
+import friends from './FriendList/friends.json';
+import FriendList from './FriendList/FriendList';
+
+import transactions from './TransactionHistory/transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 export default function App() {
   return (
@@ -12,9 +20,9 @@ export default function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={data} />
+      <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />;
-      <TransactionHistory items={transactions} />; */}
+      <TransactionHistory items={transactions} />;
     </div>
   );
 }
